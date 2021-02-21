faae26c44cf397610bedc5f76280b6a2b58322aa
Gary Isufi <isufi.2@buckeyemail.osu.edu>
2021-02-21 14:56:47 -0500

    site changes

---
+++ gary/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+
+    // spawn the title at random on screen
+    // $("#title").css({
+    //     position: "absolute",
+    //     left: Math.round(Math.random() * window.innerWidth) + "px",
+    //     top: Math.round(Math.random() * window.innerHeight) + "px",
+    // }); 
+
+    $("input[placeholder]").each(function () {
+        $(this).attr('size', $(this).attr('placeholder').length);
+    });
+
+    var input = document.getElementById('in');
+    input.focus();
+
+    // Working with input text
+    input.addEventListener("keydown", function (e) {
+        if (e.keyCode == 13) {
+            // Enter key is pressed
+            checkInput(e);
+        }
+    });
+
+    function checkInput(e) {
+        var inputText = e.target.value;
+        
+        if (inputText.toLowerCase() == "gary isufi" ) {
+            // go to main.html
+            location.href = 'main.html';
+        } else if (inputText.toLowerCase() == "tobey maguire") {
+            alert("Welcome to the lair ---- coming soon!");
+            // go to secret fun page
+        } else if (inputText.toLowerCase() == "heesoo lee") {
+            alert("Heesoo, leave my site.");
+        }
+    }
+});
