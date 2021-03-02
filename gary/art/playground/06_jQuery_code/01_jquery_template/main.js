bd03c635ccbf734baea28d161e5058f0ec8848e4
Gary Isufi <isufi.2@buckeyemail.osu.edu>
2021-03-02 00:36:29 -0500

    removed gary folder to clean files

--- gary/art/playground/06_jQuery_code/01_jquery_template/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  
-  // put javascript code here
-  console.log("Hello 3001");
-  
-  
-});
--- gary/art/playground/06_jquery_setInterval/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  
-  setInterval(appendText, 1000);
-  
-  function appendText() {
-    $("body").append("<p>SOME MORE TEXT</p>");
-  }
-  
-});
